a0d146972122e346c276746eaf16721f15ac431d
Rui Chen <chenrui333@gmail.com>
2020-04-09 13:51:38 -0400

    Remove sudo from bazel.sh
    Step 12/16 : RUN sh /runner/bazel.sh
     ---> Running in 38b0b7adda72
    root is not in the sudoers file.  This incident will be reported.
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
      0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
     89  4654   89  4179    0     0  74625      0 --:--:-- --:--:-- --:--:-- 74625
    curl: (23) Failed writing body (1313 != 1396)
    root is not in the sudoers file.  This incident will be reported.
    Hit:1 https://dl.yarnpkg.com/debian stable InRelease
    Hit:2 http://archive.ubuntu.com/ubuntu bionic InRelease
    Hit:3 http://security.ubuntu.com/ubuntu bionic-security InRelease
    Hit:4 http://repos.azul.com/azure-only/zulu/apt stable InRelease
    Hit:5 http://archive.ubuntu.com/ubuntu bionic-updates InRelease
    Hit:7 http://archive.ubuntu.com/ubuntu bionic-backports InRelease
    Hit:6 https://packagecloud.io/github/git-lfs/ubuntu bionic InRelease
    Reading package lists...
    Reading package lists...
    Building dependency tree...
    Reading state information...
    E: Unable to locate package bazel
    /usr/local/bin/bazelisk -> /usr/local/lib/node_modules/@bazel/bazelisk/bazelisk.js
    + @bazel/bazelisk@1.3.0
    added 1 package in 1.845s
    Testing to make sure that script performed as expected, and basic scenarios work
    Bazel was not installed
    The command '/bin/sh -c sh /runner/bazel.sh' returned a non-zero code: 1
    make: *** [docker-build] Error 1
    Makefile:7: recipe for target 'docker-build' failed

--- runner/scripts/bazel.sh
+++ runner/scripts/bazel.sh
@@ -5,8 +5,8 @@
 ################################################################################
 
 # Install bazel
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 apt-get update -y
 apt-get install -y bazel
 

2923d891a638fc5c99e7ff2bbdccff00ae453c49
Rui Chen <chenrui333@gmail.com>
2020-04-09 14:48:11 -0400

    Update default runner image

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ import (
 )
 
 const (
-	defaultRunnerImage = "chenrui/actions-runner:v2.165.2"
+	defaultRunnerImage = "chenrui/actions-runner:v2.168.0"
 	defaultDockerImage = "docker:19.03.6-dind"
 )
 

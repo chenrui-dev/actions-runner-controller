55f664e160c81938a0b74d6a1fa6f5a90a79dd84
chenrui <rui@meetup.com>
2020-04-17 18:04:09 -0400

    Update workpath for runner (#1)

--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -19,4 +19,4 @@ cd /runner
 ./config.sh --unattended --replace --name "${RUNNER_NAME}" --url "https://github.com/${RUNNER_REPO}" --token "${RUNNER_TOKEN}"
 
 unset RUNNER_NAME RUNNER_REPO RUNNER_TOKEN
-exec ./run.sh --once
+exec ./run.sh --work /home/github/work --once
